1487819b42814cf356ece689fc37b0d1cd935700
shoyimobloqulov <obloqulovshoyim1@gmail.com>
2024-11-20 18:57:37 +0500

    text-center fixed

--- resources/views/start_up/show.blade.php
+++ resources/views/start_up/show.blade.php
@@ -2,7 +2,6 @@
 @section('content')
     <div class="rts-blog-details rts-section-padding">
         <div class="container">
-            {{ $start_up }}
             <div class="row justify-content-center g-5">
                 <div class="col-lg-12 col-md-12">
                     <article class="blog-details">
@@ -10,8 +9,46 @@
                             <img src="{{ asset('storage/'.$start_up->image) }}" alt="blog post">
                         </div>
 
-                        <h3 class="blog-title">{{ $start_up['title_'.session('locale')] }}</h3>
-                        {!! $start_up['description_'.session('locale')] !!}
+                        <!-- Title -->
+                        <h3 class="blog-title">
+                            {{ $start_up['title_' . session('locale', 'en')] ?? $start_up['title_en'] }}
+                        </h3>
+
+                        <!-- Description -->
+                        <div class="blog-description">
+                            {!! $start_up['description_' . session('locale', 'en')] ?? $start_up['description_en'] !!}
+                        </div>
+
+                        <!-- Project Objective -->
+                        <div class="project-objective">
+                            <h4>{{ __('Project Objective') }}</h4>
+                            {!! $start_up['project_objective_' . session('locale', 'en')] ?? $start_up['project_objective_en'] !!}
+                        </div>
+
+                        <!-- Opportunities and Tasks -->
+                        <div class="opportunities-tasks">
+                            <h4>{{ __('Opportunities and Tasks') }}</h4>
+                            {!! $start_up['opportunities_tasks_' . session('locale', 'en')] ?? $start_up['opportunities_tasks_en'] !!}
+                        </div>
+
+                        <!-- Budget -->
+                        <div class="budget">
+                            <h4>{{ __('Budget') }}</h4>
+                            {!! $start_up['budget_' . session('locale', 'en')] ?? $start_up['budget_en'] !!}
+                        </div>
+
+                        <!-- Created Date -->
+                        <div class="created-at">
+                            <h4>{{ __('Created At') }}</h4>
+                            {{ \Carbon\Carbon::parse($start_up->created_at)->format('d M Y') }}
+                        </div>
+
+                        <!-- Updated Date -->
+                        <div class="updated-at">
+                            <h4>{{ __('Updated At') }}</h4>
+                            {{ \Carbon\Carbon::parse($start_up->updated_at)->format('d M Y') }}
+                        </div>
+
                     </article>
                 </div>
             </div>
