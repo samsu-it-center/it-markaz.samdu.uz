af012694ca553c05e213b94d0a1470f118706f02
shoyimobloqulov <obloqulovshoyim1@gmail.com>
2024-11-20 18:52:05 +0500

    btn-primary fixed

--- resources/views/software/show.blade.php
+++ resources/views/software/show.blade.php
@@ -10,23 +10,18 @@
                         <div class="blog-details__featured-image">
                             <img src="{{ asset('storage/'.$software->image) }}" alt="Software Image">
                         </div>
-
-                        <!-- Title -->
                         <h3 class="blog-title">{{ $software['name_'.session('locale')] }}</h3>
 
-                        <!-- Description -->
-                    {!! $software['description_'.session('locale')] !!}
+                        {!! $software['description_'.session('locale')] !!}
 
-                    <!-- Video Link (Optional) -->
                         @if($software->video_url)
                             <div class="video-link">
-                                <a href="{{ $software->video_url }}" target="_blank">Watch Video</a>
+                                <a class="btn btn-primary" href="{{ $software->video_url }}">Watch Video</a>
                             </div>
-                    @endif
+                        @endif
 
-                    <!-- Link to Software Website -->
                         <div class="software-link">
-                            <a href="{{ $software->link }}" target="_blank">Visit the software website</a>
+                            <a class="btn btn-primary" href="{{ $software->link }}">Visit the software website</a>
                         </div>
                     </article>
                 </div>
