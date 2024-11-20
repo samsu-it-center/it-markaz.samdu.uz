c9b44054723efb77357736f9a71877af31481617
shoyimobloqulov <obloqulovshoyim1@gmail.com>
2024-11-20 18:50:21 +0500

    $software fixed

--- resources/views/software/show.blade.php
+++ resources/views/software/show.blade.php
@@ -1,17 +1,33 @@
 @extends('layouts.app')
+
 @section('content')
     <div class="rts-blog-details rts-section-padding">
         <div class="container">
-            {{ $software }}
             <div class="row justify-content-center g-5">
                 <div class="col-lg-12 col-md-12">
                     <article class="blog-details">
+                        <!-- Featured Image -->
                         <div class="blog-details__featured-image">
-                            <img src="{{ asset('storage/'.$software->image) }}" alt="blog post">
+                            <img src="{{ asset('storage/'.$software->image) }}" alt="Software Image">
                         </div>
 
+                        <!-- Title -->
                         <h3 class="blog-title">{{ $software['name_'.session('locale')] }}</h3>
-                        {!! $software['description_'.session('locale')] !!}
+
+                        <!-- Description -->
+                    {!! $software['description_'.session('locale')] !!}
+
+                    <!-- Video Link (Optional) -->
+                        @if($software->video_url)
+                            <div class="video-link">
+                                <a href="{{ $software->video_url }}" target="_blank">Watch Video</a>
+                            </div>
+                    @endif
+
+                    <!-- Link to Software Website -->
+                        <div class="software-link">
+                            <a href="{{ $software->link }}" target="_blank">Visit the software website</a>
+                        </div>
                     </article>
                 </div>
             </div>
