f5005e5795ead2a14c8b4fabbb6ebcfdf8b7fdf5
fitrat1998 <towmatovft@gmail.com>
2025-01-21 10:18:08 +0500

    talantedstudents

--- database/migrations/2024_11_25_100802_create_categories_table.php
+++ database/migrations/2024_11_25_100802_create_categories_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name_uz');
+            $table->string('name_en');
+            $table->string('name_ru');
             $table->timestamps();
         });
     }
