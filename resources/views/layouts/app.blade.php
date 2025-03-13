49132f369e517e41f9d1f675e55090272dc35028
shoyimobloqulov <obloqulovshoyim1@gmail.com>
2025-03-13 13:52:13 +0500

    fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 
-    <title>{{ env('APP_NAME') }}</title>
+    <title>IT Markaz | SamDU</title>
 
     <!-- Custom CSS -->
     <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
